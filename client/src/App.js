215f6d9367d957722748c93baaac9098fbaee071
Ari Brenner <aribrenner@gmail.com>
2018-05-19 18:11:48 -0700

    GET request to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {things: null};
+  }
+
   render() {
+    const { things } = this.state;
+    if (!things) { return <div>loading...</div>; }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {things.map((thing, i) => <li key={i}>{thing}</li>)}
       </div>
     );
+  }
+
+  componentDidMount () {
+    axios.get('/api/things').then(res => {
+      this.setState({things: res.data})
+    });
   }
 }
 
