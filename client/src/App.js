6ee418b7dbc604fac9db39f70918f4574546bbfb
Ari Brenner <aribrenner@gmail.com>
2018-05-20 17:47:01 -0700

    add Home page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import PeopleIndex from './PeopleIndex';
 import PeopleShow from './PeopleShow';
 import PeopleEdit from './PeopleEdit';
 import PeopleNew from './PeopleNew';
+import Home from './Home';
 import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <Switch>
+            <Route exact path='/' component={Home} />
             <Route exact path='/people' component={PeopleIndex} />
             <Route exact path='/people/new' component={PeopleNew} />
             <Route exact path='/people/:id' component={PeopleShow} />
@@ -22,6 +24,8 @@ class App extends Component {
           <NavLink exact to="/people">All People</NavLink>
           &nbsp; | &nbsp;
           <NavLink exact to="/people/new">New Person</NavLink>
+          &nbsp; | &nbsp;
+          <NavLink exact to="/">Home page</NavLink>
         </div>
       </Router>
     );
---
+++ client/src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="Home">
+      This is a full stack SPA!  Express backend and React frontend!
+    </div>
+  );
+};
+
+export default Home;
